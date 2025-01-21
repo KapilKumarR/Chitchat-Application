c0b002a24dad7c72b0ad5619d9e9fe653789f1ef
KapilKumarR <kapilkumarrahul5@gmail.com>
2025-01-21 12:51:21 +0530

    added JWT auth

--- chatMain/usersApp/api/urls.py
+++ chatMain/usersApp/api/urls.py
@@ -1,5 +1,4 @@
 from rest_framework import routers
-#from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 from .views import AccountViewSet,NoteViewSet
 
 
@@ -10,10 +9,3 @@ Account_router.register(r'accounts', AccountViewSet)
 # Create the SimpleRouter and Token the ViewSet
 Note_router = routers.SimpleRouter()
 Note_router.register(r'notes', NoteViewSet, basename='notes')
-# Define the urlpatterns
-#urlpatterns = [
- #   path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-  #  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-   # path('notes/', get_note, name='get_note'),
-   # path('', include(Account_router.urls)),  # Include DefaultRouter URLs
-#]
